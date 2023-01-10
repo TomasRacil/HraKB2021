21fc304549f58e3174e5fb4133b85cd407139751
TomasRacil <tomas.racil@gmail.com>
2023-01-10 10:07:31 +0100

    text extractor beta

---
+++ Utils/text_extractor.py
@@ -0,0 +1,15 @@
+from csv import reader
+from os.path import join, realpath, dirname
+
+def get_text_by_id(text_id:str):
+    language= 'cz' # get from main...
+    with open(join(dirname(dirname(realpath(__file__))), "texts.csv"), "r", encoding="utf-8") as file:
+        texts = reader(file, delimiter=';')
+        header = []
+        header = next(texts)
+        col=header.index(language)
+        for row in texts:
+            if row[0]==text_id:
+                return row[col]
+
+get_text_by_id("bandit_entry")
